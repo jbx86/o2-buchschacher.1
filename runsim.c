aadce7ee899cd17e9dfe6ffc6439b40b9b412100
Joseph Buchschacher <jbx86@mail.umsl.edu>
2018-02-14 03:58:52 -0600

    Added parts of step 5 to runsim

--- runsim.c
+++ runsim.c
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define MAX_CANON 1024
+
 int main (int argc, char *argv[]) {
 	pid_t childpid = 0;
 	int n;
 	int pr_limit;
 	int pr_count = 0;
+	char str[MAX_CANON];
 
 	// verify simrun is being run with one argument
 	if (argc != 2) {
@@ -19,13 +22,24 @@ int main (int argc, char *argv[]) {
 		fprintf(stderr, "Argument must be an int > 0\n");
 		return 1;
 	}
-
-	fprintf(stderr, "%s ran with %d arguments\n", argv[0], (argc - 1));
 	pr_limit = atoi(argv[1]);
-	fprintf(stderr, "pr_limit: %d\n", pr_limit);
+
+	int i;
+	for (i = 1; i < pr_limit; i++)
+		if ((childpid = fork()) <= 0)
+			break;
+
+	// Run loop
+//	while (fgets(str, MAX_CANON, stdin) != NULL) {
+//
+//	}
 
 
-	//fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n", (long)getpid(), (long)getppid(), (long)childpid);
+	//if ((childpid = fork()) <= 0)
+	//	break;
+	//while (pr_count < pr_limit
+
+	fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n", i, (long)getpid(), (long)getppid(), (long)childpid);
 	return 0;
 
 }
